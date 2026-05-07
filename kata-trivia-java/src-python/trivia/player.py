d80e281f513442551c97e760eba7da0a03fef247
Andres Jaime <andrewjr@Andress-MacBook-Air.local>
2026-05-06 18:14:13 -0600

    refactor: create Player class (fields only)

---
+++ kata-trivia-java/src-python/trivia/player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    def __init__(self, name: str):
+        self.name         = name
+        self.position     = 0
+        self.coins        = 0
+        self.in_penalty_box = False
